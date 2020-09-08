41c205eb37ff58c49e2b17b10fee1ac514530b27
Matheus <matheus9bh@hotmail.com>
2020-09-08 00:59:49 -0300

    add-rating-on-barbers-index

---
+++ src/Components/BarberItem.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+import Rating from '../Components/Rating';
+
+const Area = styled.TouchableOpacity`
+    background-color: #FFFFFF;
+    margin-bottom: 20px;
+    border-radius: 20px;
+    padding: 15px;
+    flex-direction: row;
+`;
+
+const Avatar = styled.Image`
+    width: 88px;
+    height: 88px;
+    border-radius: 20px;
+`;
+
+const InfoArea = styled.View`
+    margin-left: 20px;
+    justify-content: space-between;
+`;
+
+const BarberName = styled.Text`
+    font-size: 17px;
+    font-weight: bold;
+`;
+
+const ProfileButton = styled.View`
+    width: 85px;
+    height: 26px;
+    border: 1px solid #4EADBE;
+    border-radius: 10px;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ProfileButtonText = styled.Text`
+    font-size: 13px;
+    color: #258596;
+`;
+
+
+
+export default({data}) => {
+    return(
+        <Area>
+            <Avatar source={{uri: data.avatar}} />
+            <InfoArea>
+                <BarberName>{data.name}</BarberName>
+
+                <Rating stars={data.stars} showRating={true} />
+
+                <ProfileButton>
+                    <ProfileButtonText>Ver Perfil</ProfileButtonText>
+                </ProfileButton>
+            </InfoArea>
+        </Area>
+    );
+}
--- src/Screens/Tabs/Home/index.js
+++ src/Screens/Tabs/Home/index.js
@@ -1,5 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
+
+import Api from '../../../Api';
+
 import { 
     Container,
     Scroller,
@@ -9,15 +14,61 @@ import {
     LocationArea,
     LocationInput,
     LocationFinder,
+    LoadingIcon,
+    ListArea,
  } from './styles';
 
 import SearchIcon from '../../../Assets/search.svg';
 import LocationIcon from '../../../Assets/my_location.svg';
 
+import BarberItem from '../../../Components/BarberItem';
+
 export default () => {
+    
+
     const navigation = useNavigation();
 
     const [location, setLocation] = useState('');
+    const [coords, setCoords] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [list, setList] = useState([]);
+
+    const handleLocationFinder = async () => {
+        setCoords(null);
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+        if(status === 'granted') {
+            setLoading(true);
+            setLocation('');
+            setList([]);
+
+            let information = await Location.getCurrentPositionAsync({});
+            setCoords(information.coords);
+            getBarbers();
+        } else {
+            alert('Permissão de localização negada!'); 
+        }
+    }
+
+    const getBarbers = async () => {
+        setLoading(true);
+        setList([]);
+
+        let response = await Api.getBarbers();
+        if(response.error == ''){
+            if(response.loc){
+                setLocation(response.loc);
+            }
+            setList(response.data);
+        } else {
+            alert("Erro: "+response.error);
+        }
+        setLoading(false);
+    }
+
+    useEffect (() => {
+        getBarbers();
+    }, []);
 
     return (
         <Container>
@@ -36,10 +87,19 @@ export default () => {
                         value={location}
                         onChangeText={text=>setLocation(text)}
                     />
-                    <LocationFinder>
+                    <LocationFinder onPress={handleLocationFinder}>
                         <LocationIcon width="26" height="26" fill="#FFFFFF" />
                     </LocationFinder>
                 </LocationArea>
+                {loading && 
+                    <LoadingIcon size="large" color="#FFFFFF" />
+                }
+
+                <ListArea>
+                    {list.map((item, key) => (
+                        <BarberItem key={key} data={item} />
+                    ))}
+                </ListArea>
             </Scroller>
         </Container>
     );
